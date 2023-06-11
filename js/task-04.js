2b4fac8cd92b1ddec9219c6bc425caa966118fe0
DaniilDrozd <124815236+DaniilDrozd@users.noreply.github.com>
2023-06-11 17:49:38 +0300

    today end 5 task

---
+++ js/task-04.js
@@ -0,0 +1,21 @@
+let counterValue = 0;
+const refs = {
+    spanValue: document.querySelector("#value"),
+    decrementBtn: document.querySelector('[data-action="decrement"]'),
+    incrementBtn: document.querySelector('[data-action="increment"]'),
+  };
+
+
+
+
+function decrement(){
+    counterValue -=1;
+    refs.spanValue.textContent = counterValue;
+}
+
+function increment(){
+counterValue +=1;
+refs.spanValue.textContent = counterValue;}
+
+refs.decrementBtn.addEventListener("click", decrement);
+refs.incrementBtn.addEventListener('click', increment);
---
+++ js/task-05.js
@@ -0,0 +1,13 @@
+const inputName = document.querySelector('#name-input');
+const outputName = document.querySelector('#name-output');
+
+inputName.addEventListener('input', newInput);
+
+function newInput(event) {
+    outputName.textContent = event.currentTarget.value;
+
+
+    if (outputName.textContent === ""){
+        return outputName.textContent = 'Anonymos'; 
+    }
+}
